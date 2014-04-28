ac40106ea6b7126307dd5b556fff73c5f429af26
atthorn <anton.t.thorn@nasa.gov>
2014-04-28 14:07:54 -0500

    AngularJS Fundamentals: Expressions - 0:00

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,11 @@
 
 eventsApp.controller('EventController',
   function EventController($scope) {
+    $scope.snippet = '<span class="text-danger" style="color: red">hi there</span>';
+    $scope.boolValue = true;
+    $scope.mystyle = {color: 'red'};
+    $scope.myclass= "blue";
+    $scope.buttonDisabled = true;
     $scope.event = {
       name: 'Angular Boot Camp',
       date: '5/1/2014',
@@ -14,15 +19,38 @@ eventsApp.controller('EventController',
       imageUrl: 'img/angularjs-logo.png',
       sessions: [
         {
-          name: 'Directives Masterclass'
+          name: 'Directives Masterclass',
+          creatorName: 'Bob Smith',
+          duration: '1 hour',
+          level: 'Advanced',
+          abstract: 'In this session, you will learn the ins and outs of directives!',
+          upVoteCount: 0
         },
         {
-          name: 'Scopes for fun and profit'
+          name: 'Scopes for fun and profit',
+          creatorName: 'John Doe',
+          duration: '30 minutes',
+          level: 'Introductory',
+          abstract: 'This session will take a closer look at scopes.  Learn what they do, how they do it, and how to get them to do it for you.',
+          upVoteCount: 0
         },
         {
-          name: 'Well-Behaved Controllers'
+          name: 'Well-Behaved Controllers',
+          creatorName: 'Jane Doe',
+          duration: '2 hours',
+          level: 'Intermediate',
+          abstract: 'Controllers are the beginning of everything Angular does; learn how to craft controllers that will win the respect of your friends and neighbors.',
+          upVoteCount: 0
         }
       ]
+    }
+
+    $scope.upVoteSession = function(session) {
+      session.upVoteCount++;
+    }
+
+    $scope.downVoteSession = function(session) {
+      session.upVoteCount--;
     }
   }
 );
